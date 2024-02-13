b0ef4c5c6293cf249ad777a541a29c7fe075534f
sergeevta <lolo_8@mail.ru>
2024-02-13 11:56:38 +0300

    yop

--- alim9.2.24.py
+++ alim9.2.24.py
@@ -1,7 +1,16 @@
-class System:
+class Student:
     pass
 
 f = open('students.csv')
-f = f.readline()
-
-print(f)
+f.readline()
+students = []
+for i in f:
+    info = Student()
+    inp = i.split(',')
+    info.Id = int(inp[0])
+    info.Fio = inp[1]
+    info.Project_id = int(inp[2])
+    info.Class = inp[3]
+    info.score = inp[::-1][4]
+    students.append(info)
+    print(inp)
