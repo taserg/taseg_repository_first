a869d0293f1ea288747869198e37fbc6b93af8e4
sergeevta <lolo_8@mail.ru>
2024-02-13 12:41:37 +0300

    fa

--- alim9.2.24.py
+++ alim9.2.24.py
@@ -1,5 +1,13 @@
 class Student:
     pass
+def sort_by_score(students):
+    for k in range(1, len(students)):
+        temp = students[k]
+        j = k - 1
+        while (j >= 0 and int(temp.Score) < int(students[j].Score)):
+            students[j + 1] = students[j]
+            j = j - 1
+        students[j + 1] = temp
 
 f = open('students.csv')
 f.readline()
@@ -11,6 +19,10 @@ for i in f:
     info.Fio = inp[1]
     info.Project_id = int(inp[2])
     info.Class = inp[3]
-    info.score = inp[::-1][4]
+    info.Score = inp[::-1][4]
     students.append(info)
-    print(inp)
+
+sort_by_score(students)
+for j in students:
+    if '10' in j.Class:
+        print(students.index(j), j.Fio, j.Class, j.Score)
