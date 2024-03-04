715c8f0fa722c19df0943624d9883a2077b06fc1
artvs <lolo_8@mail.ru>
2024-03-04 23:47:52 +0300

    zsdf132

--- alim9.2.24.py
+++ alim9.2.24.py
@@ -4,22 +4,22 @@ def sort_by_score(students):
     for k in range(1, len(students)):
         temp = students[k]
         j = k - 1
-        while (j >= 0 and int(temp.Score) < int(students[j].Score)):
+        while (j >= 0 and temp.Score < students[j].Score):
             students[j + 1] = students[j]
             j = j - 1
         students[j + 1] = temp
 
-f = open('students.csv')
-f.readline()
+f = open('students.csv', encoding="utf8")
+t = f.readlines()
 students = []
-for i in f:
+for i in t:
     info = Student()
     inp = i.split(',')
-    info.Id = int(inp[0])
+    info.Id = inp[0]
     info.Fio = inp[1]
-    info.Project_id = int(inp[2])
+    info.Project_id = inp[2]
     info.Class = inp[3]
-    info.Score = inp[::-1][4]
+    info.Score = inp[4]
     students.append(info)
 
 sort_by_score(students)
